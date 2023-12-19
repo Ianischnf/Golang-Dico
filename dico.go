d6646cdb5b0cf7e18a804edbcfd671ba16c7813e
SESAME\i_chenn <ianis.chennaf@infotel.com>
2023-12-19 12:33:28 +0100

    dico

---
+++ dico.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// Dictionary est le dictionnaire avec des mots et des définitions.
+type Dictionary map[string]string
+
+// crée un nouveau dictionnaire vide.
+func NewDictionary() Dictionary {
+	return make(Dictionary)
+}
+
+// Add pour ajouter un mot et sa définition au dictionnaire.
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
+
+// Get récupère la définition d'un mot du dictionnaire.
+func (d Dictionary) Get(word string) (string, bool) {
+	definition, exists := d[word]
+	return definition, exists
+}
+
+// Remove supprime un mot et sa définition du dictionnaire.
+func (d Dictionary) Remove(word string) {
+	delete(d, word)
+}
+
+// List renvoie une liste triée des mots et de leurs définitions.
+func (d Dictionary) List() []string {
+	var result []string
+	for word, definition := range d {
+		result = append(result, fmt.Sprintf("%s: %s", word, definition))
+	}
+	sort.Strings(result)
+	return result
+}
+
+func main() {
+	// Créez un nouveau dictionnaire.
+	dictionary := NewDictionary()
+
+	// Ajoutez quelques mots et définitions au dictionnaire.
+	dictionary.Add("pomme", "un fruit")
+	dictionary.Add("java", "un langage de programmation orienté objet")
+	dictionary.Add("Putty", "Logiciel pour se connecter en SSH à un serveur distant")
+
+	// Utilisez la méthode Get pour afficher la définition d'un mot spécifique.
+	definition, exists := dictionary.Get("golang")
+	if exists {
+		fmt.Printf("Definition de'putty': %s\n", definition)
+	} else {
+		fmt.Println("Word not found in the dictionary.")
+	}
+
+	// Utilisez la méthode Remove pour supprimer un mot du dictionnaire.
+	dictionary.Remove("apple")
+
+	// Appelez la méthode List pour obtenir la liste triée des mots et de leurs définitions.
+	list := dictionary.List()
+	fmt.Println("Dictionary entries:")
+	for _, entry := range list {
+		fmt.Println(entry)
+	}
+}

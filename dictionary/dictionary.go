a4f496144f96aa4e1a3e75808a4fb0ce4d9b0ca9
ianis-dev <ianischennaf@outlook.com>
2023-12-20 15:14:27 +0100

    dd

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,6 +1,10 @@
+// dictionary.go
 package dictionary
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"sync"
 )
 
@@ -14,17 +18,28 @@ type Dictionary struct {
 
 // Structure Entry
 type Entry struct {
-	Key   string
-	Value string
+	Key   string `json:"key"`
+	Value string `json:"value"`
 }
 
-// NewDictionary crée un nouveau dictionnaire
-func NewDictionary() *Dictionary {
-	return &Dictionary{
+// NewDictionary crée un nouveau dictionnaire à partir d'un fichier
+func NewDictionary(filename string) (*Dictionary, error) {
+	d := &Dictionary{
 		entries:    make(map[string]string),
 		addChan:    make(chan Entry),
 		removeChan: make(chan string),
 	}
+
+	// Charge les entrées à partir du fichier, s'il existe
+	err := d.loadFromFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// Lance les goroutines pour les opérations concurrentes
+	d.StartConcurrentOperations()
+
+	return d, nil
 }
 
 // Ajoute une entrée au dictionnaire
@@ -57,13 +72,89 @@ func (d *Dictionary) StartConcurrentOperations() {
 			select {
 			case entry := <-d.addChan:
 				d.mu.Lock()
-				d.entries[entry.Key] = entry.Value
+				// Vérifie si la clé existe déjà dans le dictionnaire
+				if _, exists := d.entries[entry.Key]; exists {
+					// La clé existe, met à jour la valeur associée
+					d.entries[entry.Key] = entry.Value
+				} else {
+					// La clé n'existe pas, ajoute une nouvelle entrée
+					d.entries[entry.Key] = entry.Value
+				}
 				d.mu.Unlock()
+				d.saveToFileAsync("dictionary.json")
 			case key := <-d.removeChan:
 				d.mu.Lock()
 				delete(d.entries, key)
 				d.mu.Unlock()
+				d.saveToFileAsync("dictionary.json")
 			}
 		}
 	}()
+}
+
+// Sauvegarde le dictionnaire dans un fichier de manière asynchrone
+func (d *Dictionary) saveToFileAsync(filename string) {
+	go func() {
+		d.mu.Lock()
+		defer d.mu.Unlock()
+
+		data, err := json.Marshal(d.entries)
+		if err != nil {
+			fmt.Println("Erreur lors de la sérialisation JSON :", err)
+			return
+		}
+
+		err = ioutil.WriteFile(filename, data, 0644)
+		if err != nil {
+			fmt.Println("Erreur lors de l'écriture dans le fichier :", err)
+		}
+	}()
+}
+
+// Charge le dictionnaire à partir d'un fichier
+func (d *Dictionary) loadFromFile(filename string) error {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Le fichier n'existe pas, cela peut être ignoré.
+		return nil
+	}
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	err = json.Unmarshal(data, &d.entries)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get renvoie la définition associée à une clé donnée
+func (d *Dictionary) Get(key string) (string, bool) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	value, exists := d.entries[key]
+	return value, exists
+}
+
+// Liste triée des mots et de leurs définitions
+func (d *Dictionary) List() []Entry {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	// Utiliser une map temporaire pour éviter les doublons
+	tempMap := make(map[string]string)
+
+	for key, value := range d.entries {
+		tempMap[key] = value
+	}
+
+	var list []Entry
+	for key, value := range tempMap {
+		list = append(list, Entry{Key: key, Value: value})
+	}
+
+	return list
 }
--- main.go
+++ main.go
@@ -1,47 +1,44 @@
+// main.go
 package main
 
 import (
 	"fmt"
 	"golang-dico/dictionary"
-	"sync"
-	"time"
 )
 
 func main() {
-	dict := dictionary.NewDictionary()
-	dict.StartConcurrentOperations()
-
-	var wg sync.WaitGroup
-
-	// Opérations d'ajout simultanées
-	for i := 1; i <= 5; i++ {
-		wg.Add(1)
-		go func(index int) {
-			defer wg.Done()
-			entry := dictionary.Entry{
-				Key:   fmt.Sprintf("Key%d", index),
-				Value: fmt.Sprintf("Value%d", index),
-			}
-			dict.Add(entry)
-		}(i)
+	// Créez un nouveau dictionnaire.
+	dict, err := dictionary.NewDictionary("dictionary.json")
+	if err != nil {
+		fmt.Println("Erreur lors de la création du dictionnaire :", err)
+		return
 	}
 
-	// Opérations de suppression simultanées
-	for i := 1; i <= 3; i++ {
-		wg.Add(1)
-		go func(index int) {
-			defer wg.Done()
-			key := fmt.Sprintf("Key%d", index)
-			dict.Remove(key)
-		}(i)
+	// Créez des instances de la structure Entry avec les données spécifiées
+	entry1 := dictionary.Entry{Key: "pomme", Value: "un fruit"}
+	entry2 := dictionary.Entry{Key: "java", Value: "un langage de programmation orienté objet"}
+	entry3 := dictionary.Entry{Key: "Putty", Value: "Logiciel pour se connecter en SSH à un serveur distant"}
+
+	// Ajoutez ces entrées au dictionnaire en utilisant la méthode Add
+	dict.Add(entry1)
+	dict.Add(entry2)
+	dict.Add(entry3)
+
+	// Afficher la définition d'un mot spécifique.
+	definition, exists := dict.Get("java")
+	if exists {
+		fmt.Printf("Definition de 'java': %s\n", definition)
+	} else {
+		fmt.Println("Word not found in the dictionary.")
 	}
 
-	// Attendez que toutes les goroutines se terminent
-	wg.Wait()
+	// Supprimer un mot du dictionnaire.
+	dict.Remove("java")
 
-	// Pause pour permettre la synchronisation
-	time.Sleep(100 * time.Millisecond)
-
-	// Affichez le dictionnaire final
-	fmt.Println("Dictionnaire final:", dict.Entries())
+	// Liste triée des mots et de leurs définitions.
+	list := dict.List()
+	fmt.Println("Dictionary entries:")
+	for _, entry := range list {
+		fmt.Println(entry)
+	}
 }
